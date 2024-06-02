19d341dd0792d74ef9ddb90fe9f3035fc70ddfa8
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-02 15:16:54 +0500

    add .env file to .gitignore

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const z = require('zod');
+
+router.post('/signup', (req, res) => {
+  const { name, email, password } = req.body;
+
+});
 
 module.exports = router;
