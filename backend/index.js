69e6827b31583eddd8ab89cac488d102059a5e08
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-02 14:49:17 +0500

    setup body-parser middleware. install jwt and setup its secret key

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require('./routes/index');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use('/api/v1', router);
 
