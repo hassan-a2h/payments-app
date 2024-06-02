b0795e37a804cc5866f1df0b4bb8a9c0350a6029
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-02 14:39:42 +0500

    create a separate router for handling user requests

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const userRouter = require('./routes/user');
-
-router.use('/api/v1/user', userRouter);
 
 module.exports = router;
