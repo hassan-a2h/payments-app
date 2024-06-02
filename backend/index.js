68a0319052dd888e03d4540016304af2527ff106
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-02 14:41:43 +0500

    setup cors the our app

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/v1', router);
 
