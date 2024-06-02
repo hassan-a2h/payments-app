307643f7472cc9e73617b48d21209a7a0dc5ea4f
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-06-02 14:25:00 +0500

    setup server and create basic api handler

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const router = require('./routes/index');
 
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
+
+app.listen(PORT, () => {
+  console.log('server live on port:', PORT);
+});
