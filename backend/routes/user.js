e948447423bc859c424066b841477699905a2c99
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-08-07 20:15:58 +0500

    feature: incorporate sign up logic, calling backend endpoint for user registration

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,140 +1,137 @@
 const express = require("express");
 const router = express.Router();
-const { z } = require('zod');
-const { User, Account } = require('../db');
-const jwt = require('jsonwebtoken');
-const authCheck = require('../authMiddleware');
-require('dotenv').config();
-
+const { z } = require("zod");
+const { User, Account } = require("../db");
+const jwt = require("jsonwebtoken");
+const authCheck = require("../authMiddleware");
+require("dotenv").config();
 
 //  Validation Schemas
 const userSignupSchema = z.object({
   name: z.string().min(3).max(32),
   email: z.string().email().min(6).max(64),
-  password: z.string().min(8).max(32)
+  password: z.string().min(8).max(32),
 });
 
 const userSigninSchema = z.object({
   email: z.string().email().min(6).max(64),
-  password: z.string().min(8).max(32)
+  password: z.string().min(8).max(32),
 });
 
 const userUpdateSchema = z.object({
   name: z.string().min(3).max(32),
-  password: z.string().min(8).max(32)
+  password: z.string().min(8).max(32),
 });
 
-
 //  User Routes
-router.put('/', authCheck, async (req, res) => {
+router.put("/", authCheck, async (req, res) => {
   const { name, password } = req.body;
 
-  const { id } = jwt.verify(req.cookies.Authorization.split(' ')[1], process.env.JWT_SECRET);
+  const { id } = jwt.verify(
+    req.cookies.Authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
 
   const updateFields = {};
   if (name) updateFields.name = name;
   if (password) updateFields.password = password;
 
-  const updatedUser = await User.findByIdAndUpdate(id, updateFields, { new: true });
+  const updatedUser = await User.findByIdAndUpdate(id, updateFields, {
+    new: true,
+  });
 
   if (!updatedUser) {
     return res.status(400).send({
-      message: 'User does not exist'
+      message: "User does not exist",
     });
   }
 
-  res
-    .status(200)
-    .send({ message: 'User updated successfully' });
+  res.status(200).send({ message: "User updated successfully" });
 });
 
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!userSigninSchema.safeParse({ email, password }).success) {
     return res.status(400).send({
-      message: 'Invalid user data'
+      message: "Invalid user data",
     });
   }
 
   const user = await User.findOne({ email: email });
 
-  console.log('user found:', user);
+  console.log("user found:", user);
 
   if (!user) {
     return res.status(400).send({
-      message: 'User does not exist'
+      message: "User does not exist",
     });
   }
 
   if (user.password !== password) {
     return res.status(400).send({
-      message: 'Incorrect password'
+      message: "Incorrect password",
     });
   }
 
-  console.log('user id:', typeof user._id);
+  console.log("user id:", typeof user._id);
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-  res.cookie('Authorization','Bearer ' + token, { httpOnly: true });
+  res.cookie("Authorization", "Bearer " + token, { httpOnly: true });
 
   res
     .status(200)
-    .send({ message: 'User signed in successfully',
-      token: token });
+    .send({ message: "User signed in successfully", token: token });
 });
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
     console.log(`name: ${name} email: ${email} password: ${password}`);
-  
+
     if (!userSignupSchema.safeParse({ name, email, password }).success) {
       return res.status(400).send({
-        message: 'Invalid user data'
+        message: "Invalid user data",
       });
     }
-  
+
     const user = await User.findOne({ email: email });
-  
-    console.log('Signup route, Already found user in DB:', user);
-  
+
+    console.log("Signup route, Already found user in DB:", user);
+
     if (user) {
       return res.status(400).send({
-        message: 'User already exists'
+        message: "User already exists",
       });
     }
-  
+
     const newUser = await User.create({
       name: name,
       email: email,
-      password: password
+      password: password,
     });
 
     const account = await Account.create({
       user: newUser,
-      balance: Math.ceil(Math.random() * 9999)
+      balance: Math.ceil(Math.random() * 9999),
     });
-  
+
     if (newUser && account) {
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-  
+
       res
         .status(200)
-        .send({ message: 'User created successfully',
-          token: token });
+        .send({ message: "User created successfully", accountCreated: true });
     }
-  } catch(err) {
-    console.log('Sign up route crashed, full error - ', err);
-    return res
-      .status(500)
-      .json({ message: 'Server Crashed!'});
+  } catch (err) {
+    console.log("Sign up route crashed, full error - ", err);
+    return res.status(500).json({ message: "Server Crashed!" });
   }
 });
 
-router.get('/bulk', authCheck, async (req, res) => {
+router.get("/bulk", authCheck, async (req, res) => {
   const { name, email } = req.query;
 
   const query = {};
@@ -142,19 +139,15 @@ router.get('/bulk', authCheck, async (req, res) => {
   if (name) query.name = name;
   if (email) query.email = email;
 
-  console.log('query:', query);
+  console.log("query:", query);
 
   const users = await User.find(query);
 
   if (users.length > 0) {
-    return res
-    .status(200)
-    .send({ users });
-  } 
+    return res.status(200).send({ users });
+  }
 
-  return res
-    .status(400)
-    .send({ message: 'No users found' });
+  return res.status(400).send({ message: "No users found" });
 });
 
 module.exports = router;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,51 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Signup() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  // Helper functions
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (inputs.password !== inputs.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    console.log("backend address: ", import.meta.env.VITE_BACKEND_URL);
+
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_BACKEND_URL + "/api/v1/user/signup",
+        inputs
+      );
+
+      const { accountCreated } = response.data;
+
+      if (accountCreated) {
+        alert("Account created successfully");
+        navigate("/signin");
+      }
+    } catch (error) {
+      alert(error.response.data);
+    }
+  }
+
   return (
     <>
       <div className="h-screen h-min-content w-screen w-min-content flex flex-row items-center justify-center bg-gray-500 pointer-events-none">
@@ -7,7 +54,29 @@ function Signup() {
             <h1 className="text-3xl font-bold font-sans">Sign Up</h1>
           </div>
 
-          <form action="" className="flex flex-col justify-center">
+          <form
+            onSubmit={handleSubmit}
+            action=""
+            className="flex flex-col justify-center"
+          >
+            <div className="mb-4">
+              <label htmlFor="name" className="font-semibold text-slate-700">
+                Full Name
+              </label>
+              <br />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={inputs.name}
+                onChange={handleChange}
+                placeholder="John Doe"
+                minLength={3}
+                maxLength={32}
+                className="w-[320px] py-1 px-2 mt-1 rounded-md border-2 border-slate-200"
+              />
+            </div>
+
             <div className="mb-4">
               <label htmlFor="email" className="font-semibold text-slate-700">
                 Email
@@ -17,6 +86,9 @@ function Signup() {
                 type="email"
                 name="email"
                 id="email"
+                value={inputs.email}
+                onChange={handleChange}
+                maxLength={64}
                 placeholder="user@abc.com"
                 className="w-[320px] py-1 px-2 mt-1 rounded-md border-2 border-slate-200"
               />
@@ -34,6 +106,9 @@ function Signup() {
                 type="password"
                 name="password"
                 id="password"
+                value={inputs.password}
+                onChange={handleChange}
+                minLength={8}
                 placeholder="Password"
                 className="w-[320px] py-1 px-2 mt-1 rounded-md border-2 border-slate-200"
               />
@@ -52,6 +127,9 @@ function Signup() {
                 type="password"
                 name="confirmPassword"
                 id="confirmPassword"
+                value={inputs.confirmPassword}
+                onChange={handleChange}
+                minLength={8}
                 placeholder="Repeat Password"
                 className="w-[320px] py-1 px-2 mt-1 rounded-md border-2 border-slate-200"
               />
