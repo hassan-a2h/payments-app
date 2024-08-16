72f206a16dbcd72a4b2fcf7c1f986299fa591cb9
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-08-16 15:43:48 +0500

    http to https: upgrade server to use https, use mkcert generated certificates

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,34 @@
 const express = require("express");
-const router = require('./routes/index');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const router = require("./routes/index");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
+const options = {
+  key: fs.readFileSync(`${__dirname}/certificates/localhost-key.pem`),
+  cert: fs.readFileSync(`${__dirname}/certificates/localhost.pem`),
+};
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(bodyParser.json());
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Server is active</h1>')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>HTTPS Server is active</h1>");
+});
 
-app.listen(PORT, () => {
-  console.log('server live on port:', PORT);
+https.createServer(options, app).listen(PORT, () => {
+  console.log("HTTPS server running on port " + PORT);
 });
